e70f26d0861cf7fcbd394dd8a0b69993a5347069
liorf077 <liorf077@gmail.com>
2023-02-10 10:11:25 +0200

    split to functions, code cleanup

--- GDEnumerator.py
+++ GDEnumerator.py
@@ -9,8 +9,11 @@ from googleapiclient.errors import HttpError
 ### GLOBALS ###
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-def main():
-    
+def get_creds():
+    """
+    This function generates credentials in the name of the user, 
+    and saves it for next use 
+    """ 
     # Generate token and allow the app to access the user's GDrive account
     creds = None
     if os.path.exists('token.json'):
@@ -24,54 +27,64 @@ def main():
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
-    try:
-        # create drive api client
-        service = build('drive', 'v3', credentials=creds)
+    return creds
 
-        # Get the email address of the current user
-        about = service.about().get(fields='user').execute()
-        user_email = about["user"]["emailAddress"]
-        
-        # Keep track of the last checked file
-        last_checked_file = None
+def monitor(service, user_email):
+    """
+    This function constantly monitoring the user's Google Drive 
+    to find exposed files and make them private 
+    """ 
+    # Keep track of the last checked file
+    last_checked_file = None
 
-        # Continuously check for new files
-        while True:
-            # Search query for public folders owned by the user
-            query = "mimeType='application/vnd.google-apps.folder' and (visibility='anyoneWithLink' or visibility='anyoneCanFind') and 'me' in owners"
-            try:
-                results = service.files().list(q=query,
-                                            fields='nextPageToken, '
-                                            'files(id, name)'
-                                            ).execute()
-                files = results.get("files", [])
-                for file in files:
-                    # Check if this file is a new file
-                    if last_checked_file is None or file["id"] != last_checked_file:
-                        last_checked_file = file["id"]
-                        # Extract visibility type
-                        permissions = service.permissions().list(fileId=last_checked_file).execute()
-                        visibility = permissions['permissions'][0]['id']
+    # Continuously check for new files
+    while True:
+        # Search query for public folders owned by the user
+        query = "mimeType='application/vnd.google-apps.folder' and (visibility='anyoneWithLink' or visibility='anyoneCanFind') and 'me' in owners"
+        try:
+            results = service.files().list(q=query,
+                                        fields='nextPageToken, '
+                                        'files(id, name)'
+                                        ).execute()
+            files = results.get("files", [])
+            for file in files:
+                # Check if this file is a new file
+                if last_checked_file is None or file["id"] != last_checked_file:
+                    last_checked_file = file["id"]
+                    # Extract visibility type
+                    permissions = service.permissions().list(fileId=last_checked_file).execute()
+                    visibility = permissions['permissions'][0]['id']
 
-                        try:                
-                            # Remove existing visibility
-                            service.permissions().delete(fileId=last_checked_file, permissionId=visibility).execute()
+                    try:                
+                        # Remove existing visibility
+                        service.permissions().delete(fileId=last_checked_file, permissionId=visibility).execute()
 
-                            # Change the visibility to the current user
-                            new_permission = {'type': 'user', 'role': 'owner', 'emailAddress': user_email}
-                            service.permissions().create(fileId=last_checked_file, body=new_permission, transferOwnership=True).execute()
+                        # Change the visibility to the current user
+                        new_permission = {'type': 'user', 'role': 'owner', 'emailAddress': user_email}
+                        service.permissions().create(fileId=last_checked_file, body=new_permission, transferOwnership=True).execute()
 
-                            # Notify about visibility change
-                            print(f'Visibility for file {file["name"]} has been changed to PRIVATE from {visibility}.')
-                
-                        except HttpError as error:
-                            print(f'{error}')
+                        # Notify about visibility change
+                        print(f'Visibility for file {file["name"]} has been changed to PRIVATE from {visibility}.')
+            
+                    except HttpError as error:
+                        print(f'{error}')
 
-            except HttpError as error:
-                print(f'{error}')
+        except HttpError as error:
+            print(f'{error}')
 
-    except HttpError as error:
-        print(f'{error}')
+def main():
+    # Generate credentials for the current user
+    creds = get_creds()
+    
+    # create drive api client
+    service = build('drive', 'v3', credentials=creds)
+
+    # Get the email address of the current user
+    about = service.about().get(fields='user').execute()
+    user_email = about["user"]["emailAddress"]
+    
+    # Monitor the user's Drive
+    monitor(service, user_email)
 
 if __name__ == '__main__':
     main()
