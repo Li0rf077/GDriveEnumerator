922bd1b1ca1f0038c622ce74dab475f160278687
liorf077 <liorf077@gmail.com>
2023-02-10 09:44:19 +0200

    added visibility change

--- GDEnumerator.py
+++ GDEnumerator.py
@@ -7,8 +7,10 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 ### GLOBALS ###
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.metadata']
 
+def change_scope(file):
+    pass
 def main():
     # Generate token and allow the app to access the user's GDrive account
     creds = None
@@ -23,21 +25,43 @@ def main():
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
-
     try:
         # create drive api client
         service = build('drive', 'v3', credentials=creds)
 
+        # Get the email address of the current user
+        about = service.about().get(fields='user').execute()
+        user_email = about["user"]["emailAddress"]
+
         # Search for public files
-        query = "visibility='anyoneCanFind' or visibility='anyoneWithLink'"
+        query = "mimeType='application/vnd.google-apps.folder' and (visibility='anyoneWithLink' or visibility='anyoneCanFind') and 'me' in owners"
         try:
             results = service.files().list(q=query,
                                             fields='nextPageToken, '
-                                            'files(id, name, createdTime)'
+                                            'files(id, name, createdTime, mimeType)'
                                             ).execute()
             files = results.get("files", [])
+
             for file in files:
-                print(f'{file["name"]}, Created Time: {file["createdTime"]}')
+                file_id = file['id']
+                
+                # Extract visibility type
+                permissions = service.permissions().list(fileId=file_id).execute()
+                permissionid = permissions['permissions'][0]['id']
+
+                try:                
+                    # Remove existing visibility
+                    service.permissions().delete(fileId=file_id, permissionId=permissionid).execute()
+
+                    # Add a new permission with role 'owner'
+                    new_permission = {'type': 'user', 'role': 'owner', 'emailAddress': user_email}
+                    service.permissions().create(fileId=file_id, body=new_permission, transferOwnership=True).execute()
+
+                    # Notify about permission change
+                    print(f'Visibility for file {file["name"]} has been changed to PRIVATE from {permissionid}.')
+
+                except HttpError as error:
+                    print(f'{error}')
 
         except HttpError as error:
             print(f'{error}')
